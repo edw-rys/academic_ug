3af313d197d39397e11e856537bc16a8178e3c45
Edw <edw-toni@hotmail.com>
2021-01-22 10:54:44 -0500

    comment

--- app/DataTables/PeriodDataTable.php
+++ app/DataTables/PeriodDataTable.php
@@ -33,7 +33,7 @@ class PeriodDataTable extends DataTable
      */
     public function query(Period $model): Builder
     {
-        return $model->newQuery()->orderBy('start_date','asc');
+        return $model->newQuery()->orderBy('start_date','desc');
     }
 
     /**
