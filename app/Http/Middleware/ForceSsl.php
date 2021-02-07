c35bf5ba9a7223f237abf2142c70c0e45813d58c
edw-rys <edw-toni@hotmail.com>
2021-02-06 22:20:33 -0500

    data

---
+++ app/Http/Middleware/ForceSsl.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceSsl
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        app('request')->server->set('HTTPS', true); 
+        // For Proxies
+        $request->setTrustedProxies([$request->getClientIp()], Request::HEADER_X_FORWARDED_ALL);
+
+        if (! $request->isSecure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
